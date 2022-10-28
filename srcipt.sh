045fd4e2b6397d23f2c7adf982c83ab037860fd2
kernel_loophole <p190119@nu.edu.pk>
2022-10-28 18:30:22 +0500

    Cid files

--- srcipt.sh
+++ srcipt.sh
@@ -1,2 +1,12 @@
+# docker container 
+#Container state and dependencies
+MAILER_CID=$(docker run -d dockerinaction/ch2_mailer)
+WEB_CID=$(docker create nginx)
+#The reason neither the NGINX nor the agent was included with the output has to do
+#with container state.
+AGENT_CID=$(docker create --link $WEB_CID:insideweb \
+--link $MAILER_CID:insidemailer \
+dockerinaction/ch2_agent)
+
 cid=$(docker create nginx)
 echo $cid
